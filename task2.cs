3b4703cd10800c726df063728cd762b02aef36fe
timbokido <39024286+timbokido@users.noreply.github.com>
2023-07-21 13:12:39 +0200

    Add files via upload

--- task2.cs
+++ task2.cs
@@ -1,13 +1,19 @@
-﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+﻿// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
 Console.Write("первое число = ");
 int a = Convert.ToInt32(Console.ReadLine());
 Console.Write("второе число = ");
 int b = Convert.ToInt32(Console.ReadLine());
-if (a > b) 
+Console.Write("третее число = ");
+int c = Convert.ToInt32(Console.ReadLine());
+if ((a > b) & (a > c))
 {
 Console.WriteLine("max = " + a);
 }
-else
+if ((b > a) & (b > c))
 {
-    Console.WriteLine("max = " + b);
+Console.WriteLine("max = " + b);
+}
+if ((c > b) & (c > a))
+{
+Console.WriteLine("max = " + c);
 }
