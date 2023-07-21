062be4abf75b8cc428eff3b60dd1393f66a7228a
timbokido <39024286+timbokido@users.noreply.github.com>
2023-07-21 13:35:42 +0200

    Add files via upload

---
+++ task4.cs
@@ -0,0 +1,8 @@
+﻿//  Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+Console.Write("Ваше число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int i = 2;
+while (i <= a) {
+    Console.WriteLine(i);
+    i = i + 2;
+}
