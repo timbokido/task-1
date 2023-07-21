a70ce650d1af9701c74796eca24d15ddbce81faf
timbokido <39024286+timbokido@users.noreply.github.com>
2023-07-21 13:15:36 +0200

    Update task1.cs

--- task1.cs
+++ task1.cs
@@ -6,8 +6,10 @@ int b = Convert.ToInt32(Console.ReadLine());
 if (a > b) 
 {
 Console.WriteLine("max = " + a);
+Console.WriteLine("min = " + b);
 }
 else
 {
     Console.WriteLine("max = " + b);
+    Console.WriteLine("min = " + a);
 }
